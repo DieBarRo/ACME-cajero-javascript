1aa7463205244fb195d21ab7db544a55398e3da7
Diego-Barrero <dbarrero.789@gmail.com>
2025-01-22 10:04:24 -0500

    feat: :sparkles: added way to add new users

--- javascript/index.js
+++ javascript/index.js
@@ -1,153 +1,105 @@
 
-// menu de inicio
-do {
-    let menu = `
---------------------------------------------
--------------MENU PRINCIPAL-----------------
---------------------------------------------
-(1). Crear una cuenta bancaria
-(2). Consignar dinero a una cuenta
-(3). Retirar dinero
-(4). Pagar servicios
-(5). Mostrar movimientos bancarios
-(0). Cerrar programa
+// // declaracion de objetos json para el manejo de usuarios y movimientos
 
-Elija una opcion a realizar: 
-`
+// const usuarios = {
+//     1010: {
+//         nombre: "diego",
+//         clave: "123",
+//         nCuenta: 1,
+//         dinero: 0,
+//     },
+//     2020: {
+//         nombre: "felipe",
+//         clave: "456",
+//         nCuenta: 2,
+//         dinero: 0,
+//     },
+// }
 
-    opc = Number(prompt(menu))
+// // const usuarios = {
 
-    switch(opc){
-        case 1:
-            console.log("(1). Crear una cuenta bancaria")
-            break
-        case 2:
-            console.log("(2). Consignar dinero a una cuenta")
-            break
-        case 3:
-            console.log("(3). Retirar dinero")
-            break
-        case 4:
-            console.log("(4). Pagar servicios")
-            break
-        case 5:
-            console.log("(5). Mostrar movimientos bancarios")
-            break
-        case 0:
-            window.alert("Cerrando programa")
-            break
-        default:
-            window.alert("la opcion ingresada no es valida, ingrese un numero entre 0 y 5")
+// // }
+
+
+// // menu de inicio
+// do {
+//     let menu = `
+// --------------------------------------------
+// -------------MENU PRINCIPAL-----------------
+// --------------------------------------------
+// (1). Crear una cuenta bancaria
+// (2). Consignar dinero a una cuenta
+// (3). Retirar dinero
+// (4). Pagar servicios
+// (5). Mostrar movimientos bancarios
+// (0). Cerrar programa
+
+// Elija una opcion a realizar: 
+// `
+
+//     opc = Number(prompt(menu))
+
+//     switch(opc){
+//         case 1:
+//             console.log("(1). Crear una cuenta bancaria")
+//             crearCuenta()
+
+//             const jsonObj = JSON.stringify(usuarios, 4)
+//             console.log(jsonObj)
+//             break
+//         case 2:
+//             console.log("(2). Consignar dinero a una cuenta")
+//             break
+//         case 3:
+//             console.log("(3). Retirar dinero")
+//             break
+//         case 4:
+//             console.log("(4). Pagar servicios")
+//             break
+//         case 5:
+//             console.log("(5). Mostrar movimientos bancarios")
+//             break
+//         case 0:
+//             window.alert("Cerrando programa")
+//             break
+//         default:
+//             window.alert("la opcion ingresada no es valida, ingrese un numero entre 0 y 5")
                 
-        }
+//         }
             
-    } while (opc !=0);
+//     } while (opc !=0);
               
-       
-       
-// console.log(window.confirm("algo"))
+// function crearCuenta() {
+//     const cedula = prompt("ingrese su cedula: ")
+//     const nombre = prompt("ingrese su nombre: ")
+//     const clave = prompt("ingrese su clave: ")
+//     const nCuenta = Object.keys(usuarios).length + 1
 
+//     usuarios[cedula] = {
+//             nombre: nombre,
+//             clave: clave,
+//             nCuenta: nCuenta,
+//             dinero: 0,
+//         }
+// }
 
-function nombre(){
+const username = prompt("Ingrese nombre de usuario a buscar: ")
 
+const archivo = fetch("usuarios.json");
+
+archivo.then( async response => {
+    const data = await response.json()
+    const usuario = data.find(
+        elemento => elemento.nombre == username
+    );
+    console.log(usuario)
+})
+
+async function cargarData() {
+    const archivo = await fetch("usuarios.json");
+    const data = await archivo.json()
+    const usuario = data.find(
+        elemento => elemento.nombre == username
+    );
+    console.log(usuario)
 }
-
-
-fs = require('fs')
-
-
-// #Modulo para abrir, modificar y todos los json y txt
-
-// import json
-
-
-// #abrir el json de productos y retorna los productos que son una lista de diccionarios
-// #si no existe el archivo crea la estructura basica y la retorna
-// def leerProductos():
-//     try:
-//         with open("productos.json", "r") as file:
-//             productos = json.load(file)
-//             return productos
-//     except FileNotFoundError:
-//         productos = [
-
-//         ]
-//         jsonProductos = json.dumps(productos, indent=4)
-//         with open("productos.json", "w") as file:
-//             file.write(jsonProductos)
-//             return productos
-
-
-// #abrir el json de inventario y retorna el inventario que es un diccionario de diccionarios 
-// #si no existe el archivo crea la estructura basica y la retorna
-// def leerinventario():
-//     try:
-//         with open("inventario.json", "r") as file:
-//             inventario = json.load(file)
-//             return inventario
-//     except FileNotFoundError:
-//         inventario = {
-//             "norte": {
-//             },
-//             "centro": {
-//             },
-//             "oriente": {
-//             }
-//         }
-//         jsonInventario = json.dumps(inventario, indent=4)
-//         with open("inventario.json", "w") as file:
-//             file.write(jsonInventario)
-//             return inventario
-        
-
-// #abrir el json de movimientos y retorna los movimientos que son un diccionario de diccionarios 
-// #si no existe el archivo crea la estructura basica y la retorna
-// def leerMovimientos():
-//     try:
-//         with open("movimientos.json", "r") as file:
-//             movimientos = json.load(file)
-//             return movimientos
-//     except FileNotFoundError:
-//         movimientos = {
-//             "norte": {
-//             },
-//             "centro": {
-//             },
-//             "oriente": {
-//             }
-//         }
-//         jsonMovimientos = json.dumps(movimientos, indent=4)
-//         with open("movimientos.json", "w") as file:
-//             file.write(jsonMovimientos)
-//             return movimientos
-
-
-// #Recibe la lista de diccionarios: productos, la convierte a un Json indentado para mayor legibilidad
-// #Y la guarda en el archivo productos.json
-// def actualizarProductos(productos):
-//     jsonProductos = json.dumps(productos, indent=4)
-//     with open("productos.json", "w") as file:
-//         file.write(jsonProductos)
-
-
-// #Recibe el diccionario de diccionarios: inventario, lo convierte a un Json indentado para mayor legibilidad
-// #Y lo guarda en el archivo inventario.json
-// def actualizarInventario(inventario):
-//     jsonInventario = json.dumps(inventario, indent=4)
-//     with open("inventario.json", "w") as file:
-//         file.write(jsonInventario)
-
-
-// #Recibe el diccionario de diccionarios: movimientos, lo convierte a un Json indentado para mayor legibilidad
-// #Y lo guarda en el archivo movimientos.json
-// def actualizarMovimientos(movimientos):
-//     jsonMovimientos = json.dumps(movimientos, indent=4)
-//     with open("movimientos.json", "w") as file:
-//         file.write(jsonMovimientos)
-
-
-// #guarda el reporte como un archivo .txt, 
-// # recibe un String con lo que registrar y el nombre del archivo en la segunda posicion
-// def guardarReporte(reporte, nombreArchivo):
-//     with open(fr"{nombreArchivo}.txt", "w") as archivo:
-// 	    archivo.write(reporte)
